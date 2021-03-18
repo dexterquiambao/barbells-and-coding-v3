d8114252db747d035958e85a95d5de1909e72c8f
Dexter Quiambao <dexterquiambao@googlemail.com>
2021-03-18 17:27:09 +0000

    Fixed responsive navbar for mobile

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,11 +4,11 @@
           $(window).scroll(function() {
             // checks if window is scrolled more than 500px, adds/removes solid class
             if($(this).scrollTop() > 550) {
-                $('.navbar').addClass('solid');
-                $('.back-to-top').addClass('visible');
+              $('.navbar').addClass('solid');
+              $('.back-to-top').addClass('visible');
             } else {
-                $('.navbar').removeClass('solid');
-                $('.back-to-top').removeClass('visible');
+              $('.navbar').removeClass('solid');
+              $('.back-to-top').removeClass('visible');
             }
 
           });
@@ -16,29 +16,29 @@
 
           // Scrolling effect for Arrow icons
           $("#scrollIcon").click(function() {
-      $('html, body').animate({
-          scrollTop: $("#about").offset().top
-      }, 1000);
-      return false;
-  });
+            $('html, body').animate({
+              scrollTop: $("#about").offset().top
+            }, 1000);
+            return true;
+          });
 
           $("#nav-portfolio").click(function() {
-      $('html, body').animate({
-          scrollTop: $("#portfolio").offset().top
-      }, 1000);
-      return false;
-  });
+            $('html, body').animate({
+              scrollTop: $("#portfolio").offset().top
+            }, 1000);
+            return false;
+          });
 
           $("#nav-contact").click(function() {
-      $('html, body').animate({
-          scrollTop: $("#contact").offset().top
-      }, 1000);
-      return false;
-  });
+            $('html, body').animate({
+              scrollTop: $("#contact").offset().top
+            }, 1000);
+            return false;
+          });
 
-    $(".navbar-brand").click(function(e) {
-              e.preventDefault();
-              $.scrollTo(0, 1000);
+          $(".navbar-brand").click(function(e) {
+            e.preventDefault();
+            $.scrollTo(0, 1000);
           });
 
 
